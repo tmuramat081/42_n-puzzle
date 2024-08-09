cc5e42e975d9d902229b46baeb2aad46c7959948
tmuramat <mt15hydrangea@gmail.com>
2024-08-09 20:40:40 +0900

    add: header file

--- incs/n_puzzle.h
+++ incs/n_puzzle.h
@@ -9,4 +9,8 @@ typedef struct {
 
 #define MAX_LINE 100
 
+t_board *parse_puzzle_board(const char *filename);
+void print_puzzle_board(t_board *puzzle_board);
+void free_puzzle_board(t_board *puzzle_board);
+
 #endif
